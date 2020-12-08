4814b82228ab3eaff54baded3a375c75328b4e01
nat <nmexodus870@gmail.com>
2020-12-08 19:08:42 +0000

    Register state implemented

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 
 export default class Register extends Component {
+    constructor() {
+
+        super();
+        this.state  = {
+            email:'',
+            username :'',
+            password:'',
+            password2:'',
+            role:'',
+            errors:{}
+        };
+        this.onChange=this.onChange.bind(this);
+        // we will bind this object with ur event
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+
+        this.setState({[e.target.name]:e.target.value});
+        //when state of ur controller is changing then we are holding that changed value in state.
+    }
+    onSubmit(e) {
+
+        e.preventDefault();
+        console.log('hello from submit');
+        console.log(JSON.stringify(this.state));
+    }
     render() {
         return (
             <div className="register">
@@ -9,19 +36,19 @@ export default class Register extends Component {
                 <div className="col-md-8 m-auto">
                   <h1 className="display-4 text-center">Sign Up</h1>
                   <p className="lead text-center">Create your DevConnector account</p>
-                  <form action="create-profile.html">
+                  <form onSubmit={this.onSubmit}>
                     <div className="form-group">
-                      <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" required />
+                      <input type="text" className="form-control form-control-lg" placeholder="Name" name="username" required  value={this.state.username} onChange={this.onChange} />
                     </div>
                     <div className="form-group">
-                      <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
-                      <small classNameName="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                      <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange}/>
+                      <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                     </div>
                     <div className="form-group">
-                      <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                      <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"  value={this.state.password} onChange={this.onChange}/>
                     </div>
                     <div className="form-group">
-                      <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                      <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange} />
                     </div>
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                   </form>
@@ -29,6 +56,9 @@ export default class Register extends Component {
               </div>
             </div>
           </div>
+        
+        
         )
     }
+    
 }
