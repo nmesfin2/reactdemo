377c02fa72e25a70a4fe25f010c7a93b27f051f7
nat <nmexodus870@gmail.com>
2020-12-09 20:15:30 +0000

    function based login component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,39 +1,42 @@
-import axios from "axios";
-import React, { Component } from "react";
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
-export default class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: "",
+
+const Login = () => {
+
+    
+
+    //const [data, setterFunction]=useState({initial data})
+    const [formData, setFormData] =useState({username:'',password:''})
+
+    const {username, password} = formData;
+    //const username=formData.username;
+    //const password=formData.password
+
+    const handleChange = (event) => {
+        setFormData({...formData,[event.target.name]:event.target.value})
+      };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Final state: " + username);
+        console.log("Final state: " + password);
+         const loginUser = {
+           username: username,
+           password: password,
+         };
+         axios
+           .post("http://localhost:9005/api/auth/signin", loginUser)
+           .then((response) => {console.log(response.data)
+                 localStorage.setItem("data", JSON.stringify(response.data));
+          
+        })
+        .catch((err) => console.log(err));
     };
-  }
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    console.log("Final state: " + JSON.stringify(this.state));
-    const loginUser = {
-      username: this.state.username,
-      password: this.state.password,
-    };
-    axios
-      .post("http://localhost:9005/api/auth/signin", loginUser)
-      .then((response) => {console.log(response.data)
-            localStorage.setItem("data", JSON.stringify(response.data));
-      })
-      .catch((err) => console.log(err));
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  render() {
     return (
-      <div className="login">
+        <div className="login">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -41,15 +44,15 @@ export default class Login extends Component {
               <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form onSubmit={onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Username"
                     name="username"
-                    value={this.state.username}
-                    onChange={this.handleChange}
+                    value={username}
+                    onChange={handleChange}
                   />
                 </div>
                 <div className="form-group">
@@ -58,8 +61,8 @@ export default class Login extends Component {
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
+                    value={password}
+                    onChange={handleChange}
                   />
                 </div>
                 <input
@@ -72,6 +75,7 @@ export default class Login extends Component {
           </div>
         </div>
       </div>
-    );
-  }
+    )
 }
+
+export default Login;
