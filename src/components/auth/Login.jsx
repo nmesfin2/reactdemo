9f93a26498066d407d4a216f5f1d1bdb7c09f767
nat <nmexodus870@gmail.com>
2020-12-09 18:20:52 +0000

    class based Login component implemented

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,27 +1,77 @@
-import React, { Component } from 'react'
+import axios from "axios";
+import React, { Component } from "react";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div className="login">
-                <div className="container">
-                <div className="row">
-                    <div className="col-md-8 m-auto">
-                    <h1 className="display-4 text-center">Log In</h1>
-                    <p className="lead text-center">Sign in to your DevConnector account</p>
-                    <form action="dashboard.html">
-                        <div className="form-group">
-                        <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
-                        </div>
-                        <div className="form-group">
-                        <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
-                        </div>
-                        <input type="submit" className="btn btn-info btn-block mt-4" />
-                    </form>
-                    </div>
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Final state: " + JSON.stringify(this.state));
+    const loginUser = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    axios
+      .post("http://localhost:9005/api/auth/signin", loginUser)
+      .then((response) => {console.log(response.data)
+            localStorage.setItem("data", JSON.stringify(response.data));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Username"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                  />
                 </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
                 </div>
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
