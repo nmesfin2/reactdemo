c8eb70401e862ae5caef70a274af3987be7eb11f
nat <nmexodus870@gmail.com>
2020-12-11 15:23:46 +0000

    added redux store

--- src/App.js
+++ src/App.js
@@ -8,46 +8,49 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import sample from './components/layouts/sample';
 import PostList from './components/layouts/PostList';
+import store from './redux/store/store'
 
-const postList=[
-  {
-    id:1,
-    content : 'this world will be out of the pandemic soon',
-    user:'natty'
-  },
-  {
-    id:2,
-    content : 'hello from Natty',
-    user:'natty'
-  },
-  {
-    id:3,
-    content : 'How r u',
-    user:'natty'
-  },
-  {
-    id:4,
-    content : 'this world will be out of the pandemic soon',
-    user:'natty'
-  }
-]
+import {Provider} from 'react-redux'
 
 function App() {
   return (
-    <div className="App">
-      <PostList posts={postList}></PostList>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header></Header>
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/sample" component={sample}></Route>
+          <Footer></Footer>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
 export default App;
 
 
-{/*<Router>
-<Header></Header>
-<Route exact path="/" component={Landing}></Route>
-<Route exact path="/login" component={Login}></Route>
-<Route exact path="/register" component={Register}></Route>
-<Route exact path="/sample" component={sample}></Route>
-<Footer></Footer>
-</Router>*/}
+// const postList=[
+//   {
+//     id:1,
+//     content : 'this world will be out of the pandemic soon',
+//     user:'natty'
+//   },
+//   {
+//     id:2,
+//     content : 'hello from Natty',
+//     user:'natty'
+//   },
+//   {
+//     id:3,
+//     content : 'How r u',
+//     user:'natty'
+//   },
+//   {
+//     id:4,
+//     content : 'this world will be out of the pandemic soon',
+//     user:'natty'
+//   }
+// ]
---
+++ src/redux/store/store.js
@@ -0,0 +1,18 @@
+import {createStore, applyMiddleware} from 'redux'
+
+// createStore is used to create the store
+// appliymiddlware is used to connect thunk
+
+import {composeWithDevtools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+
+//when we will mention only folder name then it will consider the inde file
+import rootReducer from '../reducers'
+
+
+const initialState = {}
+const middleware = [thunk] 
+
+const store = createStore(rootReducer, initialState, composeWithDevtools(applyMiddleware(...middleware)))
+
+export default store;
